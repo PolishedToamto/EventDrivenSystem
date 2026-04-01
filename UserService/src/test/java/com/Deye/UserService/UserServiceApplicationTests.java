4e947a1e212356bbaf7a78762a66b535922bc871
D D <a24434@MacBook-Air.local>
2026-04-01 14:11:31 -0400

    Project Structure added

---
+++ UserService/src/test/java/com/Deye/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Deye.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
