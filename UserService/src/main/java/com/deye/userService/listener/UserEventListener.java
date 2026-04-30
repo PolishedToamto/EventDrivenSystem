81a58f1976446e2e97a6155b9fa1b938dc650290
D D <a24434@MacBook-Air.local>
2026-04-30 15:15:26 -0400

    userService log added

--- UserService/src/main/java/com/deye/userService/listener/UserEventListener.java
+++ UserService/src/main/java/com/deye/userService/listener/UserEventListener.java
@@ -3,26 +3,40 @@ package com.deye.userService.listener;
 import com.deye.userService.event.OrderCreatedEvent;
 import com.deye.userService.producer.UserEventProducer;
 import com.deye.userService.service.UserService;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserEventListener {
     private final UserService userService;
     private final UserEventProducer userEventProducer;
-    private final Logger logger;
+    private final Logger logger = LoggerFactory.getLogger(UserEventListener.class);
 
     public UserEventListener(UserService userService, UserEventProducer userEventProducer) {
         this.userService = userService;
         this.userEventProducer = userEventProducer;
-        this.logger = LoggerFactory.getLogger(UserEventListener.class);
     }
 
     @KafkaListener(topics = "order.event")
-    public void handleOrderCreated(OrderCreatedEvent event) {
-        logger.info("UserService received: " + event.getOrderId());
+    public void handleOrderCreated(ConsumerRecord<String, OrderCreatedEvent> record) {
+        OrderCreatedEvent event = record.value();
+        String correlationId = new String(
+                record.headers()
+                        .lastHeader("X-Correlation-Id")//retrieve the last header with this key
+                        .value()
+        );
+
+        MDC.put("correlationId", correlationId);
+
+        logger.info("Entering UserEventListener.handleOrderCreated()");
+        logger.info("OrderCreatedEvent received{}" + event);
+        logger.info("correlation id " + correlationId);
 
         if(!userService.isValidUser(event.getUserId())
             || !userService.isValidEmail(event.getEmail())){
@@ -31,8 +45,12 @@ public class UserEventListener {
             return;
         }
 
-        logger.info("user is valid: " + event.getUserId());
+        logger.info("user event is valid");
 
         userEventProducer.sendUserEvent(event, true);
+
+        logger.info("Exiting UserEventListener.handleOrderCreated()");
+
+        MDC.clear();
     }
 }
--- UserService/src/main/java/com/deye/userService/producer/UserEventProducer.java
+++ UserService/src/main/java/com/deye/userService/producer/UserEventProducer.java
@@ -2,19 +2,39 @@ package com.deye.userService.producer;
 
 import com.deye.userService.event.OrderCreatedEvent;
 import com.deye.userService.event.UserValidatedEvent;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class UserEventProducer {
-    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final KafkaTemplate<String, UserValidatedEvent> kafkaTemplate;
+    private final Logger logger = LoggerFactory.getLogger(UserEventProducer.class);
 
-    public UserEventProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+    public UserEventProducer(KafkaTemplate<String, UserValidatedEvent> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void sendUserEvent(OrderCreatedEvent orderCreatedEvent, boolean isValid) {
+        logger.info("Entering UserEventProducer.sendUserEvent()");
+
         UserValidatedEvent userValidatedEvent = new UserValidatedEvent(orderCreatedEvent.getOrderId(), orderCreatedEvent.getUserId(), orderCreatedEvent.getEmail(), isValid);
-        kafkaTemplate.send("user.event", userValidatedEvent);
+
+        ProducerRecord<String, UserValidatedEvent> record = new ProducerRecord<String, UserValidatedEvent>("user.event", userValidatedEvent);
+
+        String correlationId = MDC.get("correlationId");
+
+        if(correlationId != null) {
+            record.headers().add("X-Correlation-Id", correlationId.getBytes());
+        }
+
+        kafkaTemplate.send(record);
+
+        logger.info("Exiting UserEventProducer.sendUserEvent()");
     }
 }
